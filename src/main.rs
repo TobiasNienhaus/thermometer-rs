4442987eeaeacedad3a95a1dfbd1fe93c8ff8e44
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2021-04-26 17:16:07 +0200

    Add a basic test for reading temperature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use dht_lib::{Sensor, read};
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        match read(Sensor::Dht11, 14) {
+            Ok(r) => {
+                println!("Read data: Temperature {}C Humidity {}%", r.temperature, r.humidity);
+            }
+            Err(e) => {
+                eprintln!("Could not read data: {:?}", e);
+            }
+        }
+        std::thread::sleep(Duration::from_secs(4));
+    }
 }
