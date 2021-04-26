9df2b68123b229792d0bd457e2dab2e5bf4b87a2
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2021-04-26 20:45:12 +0200

    Some sensor extensions

--- src/sensors.rs
+++ src/sensors.rs
@@ -17,8 +17,8 @@ impl SensorCollection {
                 Sensor {
                     sensor: DhtSensor::Dht11,
                     pin: i,
-                    update_interval: None,
-                    description: None
+                    update_interval: Some(((i as u64) * 12) % 10),
+                    description: Some("Hello".to_owned())
                 }
             }).collect()
         }
@@ -32,6 +32,13 @@ enum DhtSensorDef {
     Dht22
 }
 
+const fn min_update_interval(sensor: &DhtSensor) -> u64 {
+    match sensor {
+        Sensor::Dht11 => 1,
+        Sensor::Dht22 => 2
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Sensor {
     #[serde(with = "DhtSensorDef")]
@@ -41,4 +48,24 @@ pub struct Sensor {
     update_interval: Option<u64>,
     #[serde(default, skip_serializing_if = "Option::is_none")]
     description: Option<String>
+}
+
+impl Sensor {
+    pub fn sensor(&self) -> &DhtSensor {
+        &self.sensor
+    }
+
+    pub fn pin(&self) -> u8 {
+        self.pin
+    }
+
+    pub fn update_interval(&self) -> u64 {
+        self.update_interval.map(|v| {
+            std::cmp::max(v, min_update_interval(&self.sensor))
+        }).unwrap_or(min_update_interval(&self.sensor))
+    }
+
+    pub fn description(&self) -> Option<&str> {
+        self.description.as_deref()
+    }
 }
