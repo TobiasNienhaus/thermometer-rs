9cfb4d59de9b7f655d72cd6fd7bf06f7cd35024e
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2021-05-05 17:25:12 +0200

    read a config file

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,22 @@ mod sensors;
 use dht_lib::read;
 use sensors::DhtSensor;
 use std::time::Duration;
+use std::path::PathBuf;
+use std::fs::OpenOptions;
+use std::io::Read;
 
 const TEST_PIN: u8 = 4;
 
 fn main() {
+    // let path: PathBuf = "~/Dokumente/thermometer-config.yaml".into();
+    // let mut file = OpenOptions::new().read(true).open(path).expect("Could not open config file");
+    // let mut config_str = String::new();
+    // file.read_to_string(&mut config).expect("Could not read config file to string");
+    //
+    // let read_conf: sensors::SensorConfig = serde_yaml::from_str(config_str.as_str())
+    //     .expect("Could not deserialize config");
+
+    // let conf = read_conf;
     let conf = sensors::SensorConfig::build(vec![
         sensors::Sensor::named_dht_11(TEST_PIN, "TEST"),
     ], 1);
@@ -14,6 +26,7 @@ fn main() {
     for line in s.lines() {
         println!("{}", line);
     }
+    return;
 
     loop {
         let now = std::time::Instant::now();
